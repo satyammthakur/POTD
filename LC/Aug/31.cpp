55956c6dc9a56b9496150da50e75fd3ad1991406
satyammthakur <satyammthakur@gmail.com>
2025-08-31 19:15:01 +0530

    started solving POTD again

---
+++ LC/Aug/31.cpp
@@ -0,0 +1,9 @@
+/*
+Sudoku Solver LC-37 , LC-Hard
+Link: 
+Prerequisties- LC 36
+
+Approach- what I thought of is using the knowledge of LC 36 where I had to find valid sudoku , and use recursion 
+to add numbers 
+
+*/
